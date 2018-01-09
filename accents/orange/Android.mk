0b3eb3de157cfe522f4a9582942c83dd53793926
Joey <joey@lineageos.org>
2018-01-09 13:40:56 +0100

    Initial commit
    Change-Id: I7cee79bb20dd11837b81b3be41ba999ebc6a317e
    Signed-off-by: Joey <joey@lineageos.org>

---
+++ accents/orange/Android.mk
@@ -0,0 +1,9 @@
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_RRO_THEME := LineageOrangeAccent
+LOCAL_CERTIFICATE := platform
+LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
+LOCAL_PACKAGE_NAME := LineageOrangeAccent
+
+include $(BUILD_RRO_SYSTEM_PACKAGE)
