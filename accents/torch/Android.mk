6fa42f039ca41338986189c199bd975e97a2de8a
Demon Singur <demonsingur@gmail.com>
2018-06-06 16:20:36 +0000

    overlays: add torch accent
    Change-Id: Iecb55463bd7a0aa2a4eb352ec383a26aff4d5c07

---
+++ accents/torch/Android.mk
@@ -0,0 +1,11 @@
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_RRO_THEME := LineageTorchAccent
+LOCAL_CERTIFICATE := platform
+LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
+LOCAL_PACKAGE_NAME := LineageTorchAccent
+
+LOCAL_PRIVATE_PLATFORM_APIS := true
+
+include $(BUILD_RRO_SYSTEM_PACKAGE)
