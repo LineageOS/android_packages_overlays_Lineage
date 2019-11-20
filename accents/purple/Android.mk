b56536e9a7d052695e08393d00264b97808dd10f
Demon000 <demonsingur@gmail.com>
2019-11-20 21:07:28 +0100

    Revert "[TMP]Kill accents"
    This reverts commit 9667d6f6324c26f0755b5096d005f361146300e6.
    
    Change-Id: I1500f4b2d9ec328208a51b16840fe7c73c7f1bb8

---
+++ accents/purple/Android.mk
@@ -0,0 +1,11 @@
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_RRO_THEME := LineagePurpleAccent
+LOCAL_CERTIFICATE := platform
+LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
+LOCAL_PACKAGE_NAME := LineagePurpleAccent
+
+LOCAL_PRIVATE_PLATFORM_APIS := true
+
+include $(BUILD_RRO_SYSTEM_PACKAGE)
---
+++ dark/Android.mk
@@ -0,0 +1,12 @@
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_RRO_THEME := LineageDarkTheme
+LOCAL_CERTIFICATE := platform
+LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
+LOCAL_PACKAGE_NAME := LineageDarkTheme
+LOCAL_OVERRIDES_PACKAGES := SysuiDarkThemeOverlay
+
+LOCAL_PRIVATE_PLATFORM_APIS := true
+
+include $(BUILD_RRO_SYSTEM_PACKAGE)
