996b82023981773d38ce9317f6cb7ca4e638ec28
Demon000 <demonsingur@gmail.com>
2019-11-20 21:53:28 +0100

    overlays: accents: re-do for compatibility with ThemePicker
    Change-Id: I23764df144be22419d6464eb763dc98f73b6fdea

--- accents/green/Android.mk
+++ accents/green/Android.mk
@@ -2,10 +2,11 @@ LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_RRO_THEME := LineageGreenAccent
+LOCAL_PRODUCT_MODULE := true
 LOCAL_CERTIFICATE := platform
 LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
 LOCAL_PACKAGE_NAME := LineageGreenAccent
 
 LOCAL_PRIVATE_PLATFORM_APIS := true
 
-include $(BUILD_RRO_SYSTEM_PACKAGE)
+include $(BUILD_RRO_PACKAGE)
